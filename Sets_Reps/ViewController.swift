0d6f1be80ee7a078cd89942a251063ce132d4a13
Kathleen Weaver <kathleen@kweaver.org>
2018-01-20 21:08:32 -0600

    Initial Commit

---
+++ Sets_Reps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sets_Reps
+//
+//  Created by Kathleen Weaver on 1/20/18.
+//  Copyright © 2018 Kathleen Weaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
